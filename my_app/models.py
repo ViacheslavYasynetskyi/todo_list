4b01a412618cc97007e068559780a9c36d098371
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-08-15 23:05:07 +0100

    add models in my_app

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    finish_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks"
+    )
